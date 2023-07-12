0da6ab9304b9e309a418a7c7fbf6c8b6ad4631b2
Owner <Owner@DESKTOP-2TPH2RF>
2023-07-12 19:23:00 +0530

    Add project files.

---
+++ Exeption_Handling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exeption_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            try
+            {
+                student.AcceptDetails("", 19);
+
+            }
+
+            catch(NameExeption ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+            
+            }   
+    }
+}
---
+++ Exeption_Handling/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exeption_Handling
+{
+    public class NameExeption:Exception
+
+    {
+        public NameExeption(string Massage):base(Massage)
+        {
+        }
+
+
+    }
+    public class Student
+    {
+        private string name;
+        private int age;
+
+        public void AcceptDetails(string name,int age)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                throw new NameExeption("name is required");
+
+            }
+
+            else
+            {
+                this.name= name; 
+                this.age= age;
+            }
+        }
+        
+    }
+
+
+}
+
